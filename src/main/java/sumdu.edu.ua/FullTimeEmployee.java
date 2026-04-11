333aa57ea47ca7cdca1ca69ca50d12b53f9fda1c
NikitaTsebro <tsebro.nikita@student.sumdu.edu.ua>
2026-04-11 23:37:14 +0300

    Add derived class full time employee

---
+++ src/main/java/sumdu.edu.ua/FullTimeEmployee.java
@@ -0,0 +1,45 @@
+package sumdu.edu.ua;
+
+public class FullTimeEmployee extends Employee{
+    private int yearsInCompany;
+
+    public FullTimeEmployee() {
+        super();
+    }
+        
+    public FullTimeEmployee(String nameSurname, int age, double salary, Position position, int yearsInCompany) {
+        super(nameSurname, age, salary, position);
+        setYearsInCompany(yearsInCompany);
+    }
+
+    public FullTimeEmployee(FullTimeEmployee other) {
+        super(other);
+        this.yearsInCompany = other.yearsInCompany;
+    }
+
+    public int getYearsInCompany(){
+        return yearsInCompany;
+    }
+
+    public static void validateYearsInCompany(int yearsInCompany){
+        if(yearsInCompany < 0){throw new IllegalArgumentException("Помилка: кількість років не може бути від'ємною!");}
+    }
+
+    public void setYearsInCompany(int years){
+        validateYearsInCompany(years);
+        this.yearsInCompany = yearsInCompany;
+    }
+
+    @Override
+    public String toString() {
+        return "FullTimeEmployee{" +
+            "id=" + id +
+            ", nameSurname='" + nameSurname + '\'' +
+            ", age=" + age +
+            ", salary=" + salary +
+            ", position=" + position +
+            ", yearsInCompany=" + yearsInCompany +
+            '}';
+    }
+
+}
