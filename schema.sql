28bf0f79b4c3549ba4c634f4ba95b00d2de034cd
NikitaTsebro <tsebro.nikita@student.sumdu.edu.ua>
2026-04-28 19:42:48 +0300

    Fix schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,23 @@
-CREATE TYPE position_enum AS ENUM ('MANAGER', 'HR', 'JANITOR', 'CLERK');
-CREATE TYPE employee_type_enum AS ENUM ('FULL_TIME', 'CONTRACT', 'PART_TIME', 'INTERN');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'position_enum') THEN
+        CREATE TYPE position_enum AS ENUM ('MANAGER', 'HR', 'JANITOR', 'CLERK');
+    END IF;
+END $$;
 
-Create table COMPANY (
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'employee_type_enum') THEN
+        CREATE TYPE employee_type_enum AS ENUM ('FULL_TIME', 'CONTRACT', 'PART_TIME', 'INTERN');
+    END IF;
+END $$;
+
+Create TABLE IF NOT exists COMPANY (
 	Company_id BIGSERIAL PRIMARY KEY,
 	name Varchar(100) not null
 );
 
-CREATE TABLE EMPLOYEE (
+CREATE TABLE IF NOT exists EMPLOYEE (
     Employee_id BIGSERIAL PRIMARY KEY,
     Company_id BIGINT not null,
     Employee_type employee_type_enum NOT NULL,
